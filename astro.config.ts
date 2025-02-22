f763050cfac649b44429ca0056af022986e2a850
kaisanre <worldseekerace@gmail.com>
2025-02-22 20:43:52 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,8 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kaisanre.github.io/wahi",
+	site: "https://kaisanre.github.io",
+  base: "/wahi",
 	image: {
 		domains: ["webmention.io"],
 	},
